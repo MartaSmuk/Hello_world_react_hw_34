bcf81345aa9f62b9762877d7481aa6335ad4f1fd
Marta Smuk <msmuk999@gmail.com>
2024-08-02 00:54:24 +0200

    added project changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,35 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import UserName from './UserName.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+  //state
+  const [userName, setUserName] = useState('');
+  const [inputValue, setInputValue] = useState('');
+  
+  const doInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const doSubmit = (event) => {
+    event.preventDefault();
+    setUserName(inputValue);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <form onSubmit={doSubmit}>
+        <input
+          id="nameInput"
+          type="text"
+          value={inputValue}
+          onChange={doInputChange}
+          placeholder="Enter your name"
+         />
+         <button id="submitButton" type="submit">Ok</button>
+      </form>
+      <UserName name={userName}></UserName>
+    </div>
   )
 }
 
---
+++ src/UserName.jsx
@@ -0,0 +1,9 @@
+function UserName(pros) {
+    return (
+    <div>
+        <h1>User entered the following name: {pros.name}</h1>
+    </div>
+    );
+}
+
+export default UserName;
